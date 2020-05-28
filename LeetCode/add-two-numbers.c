4b67a50d7121bbb70dc0b270b749022e2695b998
k-chew <kailey.chew@uwaterloo.ca>
2020-05-27 17:03:26 -0700

    Update add-two-numbers.c

--- LeetCode/add-two-numbers.c
+++ LeetCode/add-two-numbers.c
@@ -13,8 +13,18 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
     struct ListNode *resultPointer = result;
     int carry = 0;
     int sum;
-    while (l1 && l2) {
-        sum = l1->val + l2->val + carry;
+    while (1) {
+        // it is possible that one number's digits are already added fully
+        // so we must check before adding to this iteration's sum
+        sum = carry;
+        if (l1) {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+        if (l2) {
+            sum += l2->val;
+            l2 = l2->next;
+        }
         if (sum >= 10) {
             resultPointer->val = sum % 10;
             carry = 1;
@@ -22,11 +32,19 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
             resultPointer->val = sum;
             carry = 0;
         }
-        l1 = l1->next;
-        l2 = l2->next;
-        resultPointer->next = malloc(sizeof(struct ListNode));
-        resultPointer = resultPointer->next;
+        if (l1 || l2) {
+          resultPointer->next = malloc(sizeof(struct ListNode));
+          resultPointer = resultPointer->next;
+        } else {
+          if (carry == 1) {
+            resultPointer->next = malloc(sizeof(struct ListNode));
+            resultPointer->next->val = 1;
+            resultPointer->next->next = NULL;
+            break;
+          }
+          resultPointer->next = NULL;
+          break;
+        }
     }
-    free(resultPointer);
     return result;
 }
